4da14b3b29ae7356c40f500dd1774ed90f280b37
Gauri2230 <108929783+Gauri2230@users.noreply.github.com>
2023-09-20 07:30:38 +0000

    converion operation

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,59 @@
+//+++++++++++++++++++++++++++++++++++++++++++++++ Conversion +++++++++++++++++++++++++++++++++++++++++++++++
+/*
+let score = "33"
+console.log(typeof score);
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+/*
+"33" => 33
+"33abc" => NaN(Not a Number)
+true => 1
+false => 0
+undefined =>NaN
+null => 0
+*/
+/*
+let isLoggedIn = 0
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+1 => true
+0 => false
+"Gauri" =>true
+"" =>false
+*/
+/*
+let number = 33
+let stringNumber = String(number)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+*/
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++ Operations +++++++++++++++++++++++++++++++++++++++
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2**2);
+console.log(2%2);
+
+let str1 = "Gauri"
+let str2 = " Malvankar"
+console.log(str1 + str2);
+
+console.log('2' + 2);
+console.log(2 + '2');
+console.log(2 + 2);
+console.log('2' + '2');
+console.log('2' + 2 + '2');
+console.log(2 + 2 + '2');
+console.log('2' + 2 + 2);
