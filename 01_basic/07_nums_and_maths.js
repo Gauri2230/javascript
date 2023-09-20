37db6cfb4ba1cec7684a94890501a59ac72d9eac
Gauri2230 <108929783+Gauri2230@users.noreply.github.com>
2023-09-20 12:33:55 +0000

    num_Maths

---
+++ 01_basic/07_nums_and_maths.js
@@ -0,0 +1,28 @@
+//++++++++++++++++++++++++ Numbers ++++++++++++++++++++++++++++++++++++
+/*
+const score = 400
+console.log(score);
+const balance = new Number(100) 
+console.log(balance);
+console.log(balance.toString());
+console.log(balance.toFixed());
+
+const num1 = 549.43122
+console.log(num1.toPrecision(4));
+
+const hundreds = 10000000
+console.log(hundreds.toLocaleString("en-IN"));
+*/
+
+
+//++++++++++++++++++++++++ Maths +++++++++++++++++++++++++++
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(2.5));
+// console.log(Math.ceil(2.5));
+// console.log(Math.floor(2.5));
+// console.log(Math.sqrt(4));
+// console.log(Math.pow(2,2));
+// console.log(Math.min(2, 8, 1, 65));
+// console.log(Math.max(2, 8, 1, 65));
+console.log(Math.random());
