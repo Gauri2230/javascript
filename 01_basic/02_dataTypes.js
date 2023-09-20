69347e3bd1386fd2ca05de50551a0309b78028b2
Gauri2230 <108929783+Gauri2230@users.noreply.github.com>
2023-09-20 07:30:05 +0000

    datatypes

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all JS code as newer version
+
+let name = "Gauri"
+let age = 23
+let isLoggedIn = false
+
+/*
+number => 2 to power 53
+bigInt
+string => ""
+boolean => true/false
+null => standalone value
+undefined =>
+symbol=>unique
+
+object
+*/
+
+console.log(typeof "Gauri");
+console.log(typeof age);
+console.log(typeof isLoggedIn);
+console.log(typeof null);  //object
+console.log(typeof undefined);  //undefined
